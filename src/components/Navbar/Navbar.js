efa52162a8c67cdb239612551daa0384c420fc6f
Tahseen Malik <hello@tahseen.me>
2018-06-25 00:02:57 +0100

    feat: Export a Navbar HOC

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,30 +4,34 @@ import AppBar from '@material-ui/core/AppBar'
 import Toolbar from '@material-ui/core/Toolbar'
 import { Link } from 'react-router-dom'
 import { withStyles } from '@material-ui/core/styles'
-import Logo from './Logo'
+import defaultLogo from './Logo'
 
-const styles = {
-  appbar: {
-    backgroundColor: '#4396ec'
-  },
-  toolbar: {
-    justifyContent: 'center',
-    alignItems: 'center'
+export const createNavbar = (color = '#4396ec', Logo = defaultLogo) => {
+  const styles = {
+    appbar: {
+      backgroundColor: color
+    },
+    toolbar: {
+      justifyContent: 'center',
+      alignItems: 'center'
+    }
   }
+
+  const Navbar = ({ classes }) => (
+    <AppBar position="static" className={classes.appbar}>
+      <Toolbar className={classes.toolbar}>
+        <Link to="/" className={styles.link}>
+          <Logo />
+        </Link>
+      </Toolbar>
+    </AppBar>
+  )
+
+  Navbar.propTypes = {
+    classes: PropTypes.object.isRequired
+  }
+
+  withStyles(styles)(Navbar)
 }
 
-const Navbar = ({ classes }) => (
-  <AppBar position="static" className={classes.appbar}>
-    <Toolbar className={classes.toolbar}>
-      <Link to="/" className={styles.link}>
-        <Logo />
-      </Link>
-    </Toolbar>
-  </AppBar>
-)
-
-Navbar.propTypes = {
-  classes: PropTypes.object.isRequired
-}
-
-export default withStyles(styles)(Navbar)
+export default createNavbar()
