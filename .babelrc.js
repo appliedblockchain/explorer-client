997e71452ef4c78de1f75dbd9d9aa2b4753b62c8
Tahseen Malik <hello@tahseen.me>
2018-07-04 14:51:01 +0100

    chore: Add babel object spread plugin

--- .babelrc.js
+++ .babelrc.js
@@ -2,5 +2,8 @@
 
 module.exports = {
   presets: ['@babel/preset-react'],
-  plugins: ['@babel/plugin-proposal-class-properties']
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-object-rest-spread'
+  ]
 }
